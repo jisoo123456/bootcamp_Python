0e0df2abf4cda874ad5c6662f2739bc2ea127f44
jisoo <imearth0913@gmail.com>
2023-01-13 16:32:45 +0900

    1 assignment

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-# 코드 수정
+import random
+
+guess=int(input("1~10사이의 숫자를 선택하여 입력하시오."))
+secret=random.randint(1,10)
+
+if secret > guess:
+    print("too low")
+
+if secret < guess:
+    print("too high")
+
+if secret == guess:
+    print("just right")
+
+print(secret)
