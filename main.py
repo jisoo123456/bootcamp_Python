019d5bbcb91799d3febb4618271eeaabe57a01d5
jisoo <imearth0913@gmail.com>
2023-01-13 16:38:33 +0900

    2 assignment

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-import random
+small=False
+green=False
 
-guess=int(input("1~10사이의 숫자를 선택하여 입력하시오."))
-secret=random.randint(1,10)
-
-if secret > guess:
-    print("too low")
-
-if secret < guess:
-    print("too high")
-
-if secret == guess:
-    print("just right")
-
-print(secret)
+if small:
+    if green:
+        print("완두콩")
+    else:
+        print("체리")
+else:
+    if green:
+        print("수박")
+    else:
+        print("호박")
