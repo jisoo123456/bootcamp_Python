a2ebea29d4e38024f0729ea0b755ae14417c413e
jisoo123456 <imearth0913@gmail.com>
2023-01-16 15:54:54 +0900

    prime number(between number)

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-#prime_number
+#입력된 두 수 사이의 소수 출력
 
-number = int(input("input number:"))
-counts = 0
+start=int(input('start number: '))
+end=int(input('end number: '))
 
-k=1
-while k <= number:
-    if number % k == 0:
-        counts = counts+1
-    k=k+1
+if end<start:
+    start, end= end, start
 
-if counts ==2 :
-    print(f'{number} is prime number')
-else:
-    print(f'{number}is NOT prime number')
+for i in range(start, end+1):
 
+    if i <=1:
+        continue
+    for k in range(2,i):
+        if i%k ==0:
+            break
+    else:
+        print(i, end=' ')
