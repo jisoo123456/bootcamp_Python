e999205a8cd715f52cbcd4ea7fb4610f9cc2c06d
jisoo123456 <imearth0913@gmail.com>
2023-01-16 16:42:30 +0900

    5.3

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-#5.2(다음과 같은 형식으로 각 리스트의 질문과 답 출력)
+#5.3
+My_Kitty="My kitty cat likes %s,\nMy kitty cat likes %s,\nMy Kitty cat fell on his %s And now thinks he's a %s."
 
-question=[
-    "We don't serve strings around here. Are you a string",
-    "What is said on Father's Day in the forest?",
-    "What makes the sound 'Sis!' 'Boom!' 'Bah!'?"
-]
-
-answers=[
-    "An exploding sheep.",
-    "No, I'm a frayed knot.",
-    "'Pop!' goes the weasel."
-]
-
-for i in range(0, len(question)):
-    print(question[i], answers[i])
+print(My_Kitty %('roast beef', 'ham','head','calm'))
