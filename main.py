a9795f63c2eaae74021fb1a5421aacb2c46bf4c1
jisoo123456 <imearth0913@gmail.com>
2023-01-16 14:32:46 +0900

    prime_number(for)

--- main.py
+++ main.py
@@ -1,32 +1,14 @@
-#song="""When an eel grabs your arm,
-#... And it causes great harm,
-#...That's - a moray!"""
+#prime_number
 
-#song_list=song.split()
-#song_list[15]=song_list[15].title()
-#song_string=' '.join(song_list)
-#print(song_string)
+number = int(input("input number:"))
+counts = 0
 
-#Song=song.replace('m','M')
-#idx=song.refind('m')
-#Song=song.replace(song[idx],song[idx.upper()])
+for k in range(1, number+1):
+        if number % k == 0:
+            counts = counts + 1
 
+if counts ==2 :
+    print(f'{number} is prime number')
+else:
+    print(f'{number}is NOT prime number')
 
-#print(song.endswith('m'))
-
-
-#구구단
-#while True:
-#    dan = int((input('Dan: ')))
-#
-#
-#    if 1 < dan < 10:
-#        i = 1
-#        while i < 10:
-#            print('{0}*{1}={2}'.format(dan, i, dan * i))
-#            i = i + 1
-#    elif dan==0:
-#        break
-#
-#    else:
-#        print('2에서 9사이의 값을 입력하세요')
