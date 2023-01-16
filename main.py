c75562e042b39a094abc636e4e36e8ee1a424a0e
jisoo123456 <imearth0913@gmail.com>
2023-01-16 17:49:02 +0900

    5.6

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-#5.5
+#5.6
 
-letter=['salutation','name','prodcut','verbed','room','animals','amount','percent','spokesman','job_title']
-
-print('''Dear {0}{1}\n\nThank you for your letter. 
-We are sorry that our {2} {3} in your {4} Please note that it should never be used in a {5}, 
-especially near any {6}.\n\nSend us your receipt and {7} for shipping and handling.
-We will send you another {8} that, in out tests, is {9} less likely to have {10}.
-\n\nThank you for your support.\nSincerly,\n{11},\n{12}'''.format(letter[0],letter[1],letter[2],letter[3],letter[4],letter[4],letter[5],letter[6],letter[2],letter[7],letter[4],letter[8],letter[9]))
-
+print("%sy Mc%sface" % ('Duck','Duck'))
+print("%sy Mc%sface" % ('Gourd','Gourd'))
+print("%sy Mc%sface" % ('Spitz','Spitz'))
 
 
