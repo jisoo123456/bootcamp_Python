068c37a32d27e5ed126e00e6089be195f4be7538
jisoo123456 <imearth0913@gmail.com>
2023-01-16 14:33:43 +0900

    prime_number(while)

--- main.py
+++ main.py
@@ -3,9 +3,11 @@
 number = int(input("input number:"))
 counts = 0
 
-for k in range(1, number+1):
-        if number % k == 0:
-            counts = counts + 1
+k=1
+while k <= number:
+    if number % k == 0:
+        counts = counts+1
+    k=k+1
 
 if counts ==2 :
     print(f'{number} is prime number')
