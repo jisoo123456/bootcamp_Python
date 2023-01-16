0a5a450d2df821625ffe2b74277f9536f71b0296
jisoo123456 <imearth0913@gmail.com>
2023-01-16 17:16:41 +0900

    5.4

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-#5.3
-My_Kitty="My kitty cat likes %s,\nMy kitty cat likes %s,\nMy Kitty cat fell on his %s And now thinks he's a %s."
+#5.4
 
-print(My_Kitty %('roast beef', 'ham','head','calm'))
+letter=['salutation','name','prodcut','verbed','room','animals','amount','percent','spokesman','job_title']
+
+print(f'''Dear {letter[0]}{letter[1]}\n\nThank you for your letter. 
+We are sorry that our {letter[2]} {letter[3]} in your {letter[4]} Please note that it should never be used in a {letter[4]}, 
+especially near any {letter[5]}.\n\nSend us your receipt and {letter[6]} for shipping and handling.
+We will send you another {letter[2]} that, in out tests, is {letter[7]} less likely to have {letter[4]}.
+\n\nThank you for your support.\nSincerly,\n{letter[8]},\n{letter[9]}''')
+
+
+
