5dd1217af03a802f539858248dfef9f813820914
jisoo123456 <imearth0913@gmail.com>
2023-01-16 14:05:41 +0900

    day03 ex

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
-small=False
-green=False
+#song="""When an eel grabs your arm,
+#... And it causes great harm,
+#...That's - a moray!"""
 
-if small:
-    if green:
-        print("완두콩")
-    else:
-        print("체리")
-else:
-    if green:
-        print("수박")
-    else:
-        print("호박")
+#song_list=song.split()
+#song_list[15]=song_list[15].title()
+#song_string=' '.join(song_list)
+#print(song_string)
+
+#Song=song.replace('m','M')
+#idx=song.refind('m')
+#Song=song.replace(song[idx],song[idx.upper()])
+
+
+#print(song.endswith('m'))
+
+
+#구구단
+#while True:
+#    dan = int((input('Dan: ')))
+#
+#
+#    if 1 < dan < 10:
+#        i = 1
+#        while i < 10:
+#            print('{0}*{1}={2}'.format(dan, i, dan * i))
+#            i = i + 1
+#    elif dan==0:
+#        break
+#
+#    else:
+#        print('2에서 9사이의 값을 입력하세요')
