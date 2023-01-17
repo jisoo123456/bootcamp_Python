8dcdc08425fd685208004c732a3f4bc6ef3377d9
jisoo123456 <imearth0913@gmail.com>
2023-01-17 16:49:05 +0900

    7.11

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-#7.10
-a_list = [number for number in range(1, 11) if number %2 == 0]
-print(a_list)
+#7.11
+start1 = ['fee', 'fie', 'foe']
+rhymes = [
+    ('floap', 'get a mop'),
+    ('floap', 'turn the roap'),
+    ('fa', 'get your ma'),
+    ('fudge', 'call the judge'),
+    ('fat', 'pet the cat'),
+    ('fog', 'walk the dog'),
+    ('fun', "say we're done")
+]
+start2 = "Someone better!"
+
+print(start1[0].title()+'!', start1[1].title()+'!', start1[2].title()+'!', rhymes[0][0].title()+'!')
+print(start2, rhymes[0][1]+'.')
