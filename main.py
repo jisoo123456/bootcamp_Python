27db360edb392a151860f242302afb1feec1f005
jisoo123456 <imearth0913@gmail.com>
2023-01-17 18:51:03 +0900

    8.10

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-#8.1~8.5
+#8.10
 
-e2f = {"dog": "chien", "cat": "chat", "walrus": "morse"}  #8.1
-print(e2f, type(e2f))
-print(e2f["walrus"])  #8.2
-
-f2e = {a: b for b, a in e2f.items()}  #8.3
-print(f2e)
-
-print(f2e['chien'])  #8.4
-
-print(list(e2f))  #8.5
-
+squares = {number: number**2 for number in range(1,11)}
+print(squares)
