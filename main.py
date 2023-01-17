d0a9f060ba85432f210d22ba577293c1f6b4e81e
jisoo123456 <imearth0913@gmail.com>
2023-01-17 16:32:06 +0900

    7.10

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-#7.8~7.9
-
-surprise = ['Groucho', 'Chico', 'Harpo']  #7.8
-surprise[2] = surprise[2].lower()
-surprise[2] = surprise[2][::-1]
-surprise[2] = surprise[2].title()
-print(surprise)
+#7.10
+a_list = [number for number in range(1, 11) if number %2 == 0]
+print(a_list)
