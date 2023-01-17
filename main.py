e860c187883d31563b86d8b9c768a6bd83af2217
jisoo123456 <imearth0913@gmail.com>
2023-01-17 16:27:50 +0900

    7.8~7.9

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-#7.4~7.7
-things = ['mozzarella', 'cinderella', 'salmonella']
-things[1] = things[1].title()  #7.5
-things[0] = things[0].upper()  #7.6
-#7.7
-#things.pop()
-#things.remove('salmonella')
-del things[2]
-print(things, type(things))
+#7.8~7.9
+
+surprise = ['Groucho', 'Chico', 'Harpo']  #7.8
+surprise[2] = surprise[2].lower()
+surprise[2] = surprise[2][::-1]
+surprise[2] = surprise[2].title()
+print(surprise)
