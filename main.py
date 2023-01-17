5806f649fc8f1331bdc7528f0f2f6c1a8cd43653
jisoo123456 <imearth0913@gmail.com>
2023-01-17 15:11:02 +0900

    alcohol_foods program

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-#dict
+#안주 추천 프로그램
+import random
+
 
 alcohol_foods={
     '맥주': '치킨',
@@ -6,24 +8,24 @@ alcohol_foods={
     '와인': '치즈',
     '고량주': '짬뽕'
 }
-alcohol_list=list(alcohol_foods)
-print(alcohol_list)
 
-foods_list = [food for food in alcohol_foods.values()]
-print(foods_list)
+alcohol_list=list(alcohol_foods)
+food_list = [food for food in alcohol_foods.values()]
 
 while True:
-    alcohol=input(f"술을 선택하세요 1){alcohol_list[0]}, 2){alcohol_list[1]}, 3){alcohol_list[2]}, 4){alcohol_list[3]}, 5)계산:")
-    if alcohol =='5':
+    alcohol = input(f"술을 선택 하세요 1){alcohol_list[0]}, 2){alcohol_list[1]}, 3){alcohol_list[2]}, 4){alcohol_list[3]}, 5)아무거나, 6)계산:")
+    if alcohol == '6':
         print("다음에 또 오세요~")
         break
-    elif alcohol =='1':
+    elif alcohol == '1':
         print(f'{alcohol_list[0]}에 어울리는 안주는 {alcohol_foods[alcohol_list[0]]}입니다.')
-    elif alcohol =='2':
+    elif alcohol == '2':
         print(f'{alcohol_list[1]}에 어울리는 안주는 {alcohol_foods[alcohol_list[1]]}입니다.')
-    elif alcohol =='3':
+    elif alcohol == '3':
         print(f'{alcohol_list[2]}에 어울리는 안주는 {alcohol_foods[alcohol_list[2]]}입니다.')
-    elif alcohol =='4':
+    elif alcohol == '4':
         print(f'{alcohol_list[3]}에 어울리는 안주는 {alcohol_foods[alcohol_list[3]]}입니다.')
+    elif alcohol == '5':
+        print(f'{random.choice(alcohol_list)}에 어울리는 안주는 {random.choice(food_list)}입니다.')
     else:
         print("메뉴에서 골라주세요.")
