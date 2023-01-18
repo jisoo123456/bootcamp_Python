de1eadb02dd820e529578ed374ae15b2cf9154a5
jisoo123456 <imearth0913@gmail.com>
2023-01-18 17:28:25 +0900

    9.1

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-# 8.14
-titles = ['Creature of Habit', 'Crewel Fate']
-plots = ['A nun turns into a mon ster', 'A haunted yarn shop']
+# 9.1
 
-a_dict = dict(zip(titles, plots))
-print(a_dict)
+def good():
+    print (['Harry', 'Ron', 'Hermione'])
 
+good()
