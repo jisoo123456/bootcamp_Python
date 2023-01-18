8ceae1f4bf9749fc4c1febd1b652f3bffd884e42
jisoo123456 <imearth0913@gmail.com>
2023-01-18 16:42:34 +0900

    8.13

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-# 8.12
-a = (number for number in range(10))
+#8.13
+a_key = ('optimist', 'pessimist', 'troll')
+a_value = ('The glas is half full', "The glass is half empty", 'How did you get a glass')
 
-for i in a:
-    print(f'Got{i}')
+a_dict = dict(zip(a_key, a_value))
+print(a_dict)
