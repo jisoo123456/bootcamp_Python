15071c94cfbe17f09d22b64699a151f54603ac55
jisoo123456 <imearth0913@gmail.com>
2023-01-19 16:13:15 +0900

    9.2

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-class Pokemon:
-    def __init__(self,name, owner, skills):  #객체 생성시 동작
-        self.name = name
-        self.owner = owner
-        self.skills = skills.split('/')
-        print(f'포켓몬 {name} 생성됨')
+# 9.2
+def get_odds():
+    for numb in range(10):
+        if numb % 2 != 0:
+            yield numb
 
-    def info(self):
-        print(f'{self.owner}의 포켓몬은 {self.name}입니다.')
-        for skill in self.skills:
-            print(skill)
+a = get_odds()
 
-
-p1 = Pokemon('피카츄', '한지우', '50만볼트/100만볼트/번개')
-p2 = Pokemon('꼬부기', '오바람', '고속스핀/거품/몸통박치기/하이드로펌프')
-p1.info()
+count = 0
+for i in get_odds():
+    count = count +1
+    if count == 3:
+        print(i)
