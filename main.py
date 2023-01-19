b12854ab12a57da5a052bbbc74e24c1149e84249
jisoo123456 <imearth0913@gmail.com>
2023-01-19 13:47:49 +0900

    except

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
-dic = {'a': 'aa', 'b': 'bb', 'c': 'cc'}
+#def div_calc(a, b):
+#   """
+#    나누기 함수
+#    :param a:분자
+#    :param b: 분모
+#    :return: 계산결과
+#    """
 
-def test(n):
-    return dic[n]
+#    return a/b
 
-i = test('c')
-print(i)
+try:
+    #raise '쉬는 시간!'
+    raise Exception('쉬는시간')
+    expr = input('분자 분모 입력(띄어쓰기로 구분): ').split()
+    print(int(expr[0]) / int(expr[1]))
+except ValueError as err:
+    print(f'숫자를 입력해주세요.({err})')
+except ZeroDivisionError as err:
+    print(f'분모에 0이 올 수 없습니다.({err})')
+except IndexError as err:
+    print(f'입력 값의 범위에 문제가 있습니다.({err})')
+except Exception as other:
+    print(f'예외발생:{other}')
+else:  #예외가 발생하지 않았을 때
+    print('프로그램 정상', end = '')
+finally:  #예외 발생 여부와 상관 없이 무조건 실행
+    print('종료')
