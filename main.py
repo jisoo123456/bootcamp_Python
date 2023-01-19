863bad6b7845dca1ccb21ca6f91ce844cd484779
jisoo123456 <imearth0913@gmail.com>
2023-01-19 10:05:38 +0900

    yield

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-# 9.1
+# generator
 
-def good():
-    print (['Harry', 'Ron', 'Hermione'])
+def a():
+    yield 1
+    yield 2
+    yield 3
 
-good()
+def b():
+    return 1  #1을 return 하고 종류/뒤에는 실행되지 않음
+    return 2
+    return 3
+
+print(a(), b())
+
+for i in a():
+    print(i)
