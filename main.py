206ac77142ac39ee203faac4b4d44ff506096c31
jisoo123456 <imearth0913@gmail.com>
2023-01-19 15:40:18 +0900

    pokemon

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-groups = {
-    '빅뱅' : ['GD', '태양', '탑', '대성', '승리'],
-    '마마무' : ['문별', '솔라', '휘인', '화사']
-}
+class Pokemon:
+    def __init__(self,name, owner, skills):  #객체 생성시 동작
+        self.name = name
+        self.owner = owner
+        self.skills = skills.split('/')
+        print(f'포켓몬 {name} 생성됨')
 
-for group, members in groups.items():
-    print(f'{group}의 멤버')
-    for member in members:
-        if member != '승리':
-            print(member)
+    def info(self):
+        print(f'{self.owner}의 포켓몬은 {self.name}입니다.')
+        for skill in self.skills:
+            print(skill)
+
+
+p1 = Pokemon('피카츄', '한지우', '50만볼트/100만볼트/번개')
+p2 = Pokemon('꼬부기', '오바람', '고속스핀/거품/몸통박치기/하이드로펌프')
+p1.info()
