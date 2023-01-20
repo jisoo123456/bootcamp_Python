86d677471c4e12a613671027c85892dcba4bf047
jisoo123456 <imearth0913@gmail.com>
2023-01-20 14:50:52 +0900

    shape

--- main.py
+++ main.py
@@ -1,40 +1,28 @@
-# class
-class Pokemon:
-    def __init__(self, owner, skills):
-        self.owner = owner
-        self.skills = skills.split('/')  #split자체가 리스트로 반환
-        print(f'포켓몬 생성됨:', end=' ')
+import math
+class Shape:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def get_area(self):
+        print('도형의 면적을 출력합니다.')
 
-    def info(self):
-        print(f"{self.owner}의 포켓몬이 사용 가능한 스킬")
-        for skill in self.skills:
-            print(skill)
+class Circle(Shape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y)
+        self.radius = radius
+    def get_area(self):
+        return math.pi * self.radius *self.radius
 
-    def attack(self, idx):
-        print(f'{self.skills[idx]} 공격 시전!')
+class Square(Shape):
+    def __init__(self, x, y, width, length):
+        super().__init__(x, y)
+        self.width = width
+        self.length = length
+    def get_area(self):
+        return math.pi * self.width * self.length
 
-class Pikachu(Pokemon):  # inheritance
-    def __init__(self, owner, skills):
-        super().__init__(owner, skills)
-        self.name = '피카츄'  #self 안 달아주면 밑에 attrack함수에서 self.name아니고 name으로 하면 못 받음
-        print(f'피카츄')
-    def attack(self, idx):
-        print(f'{self.owner}의 {self.name}가 {self.skills[idx]} 공격(전기) 시전!')
+c1 = Circle(100, 100, 10.0)
+c2 = Circle(50, 50, 2.0)
+r1 = Square(100, 50, 5, 2)
 
-class Ggoboogi(Pokemon):  # inheritance
-    def __init__(self, owner, skills):
-        super().__init__(owner, skills)
-        self.name = "꼬부기"
-        print(f'꼬부기')
-    def attack(self, idx):
-        print(f'{self.owner}의 {self.name}가 {self.skills[idx]} 공격(물) 시전!')
-    def swim(self):
-        print(f'{self.name}이 수영을 합니다.')
-
-pk1 = Pikachu('한지우', '50만 볼트/100만 볼트/번개')
-ggo1 = Ggoboogi('오바람', '고속스핀/거품/몸통박치기')
-ggo1.swim()
-# pk1.info()
-# ggo1.info()
-ggo1.attack(1)
-pk1.attack(1)
+print(f'사각형의 좌표는 x:{r1.x}, y:{r1.y}이고 넓이는 {r1.get_area()}')
