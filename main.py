fc1cd453a02c3ed98cbadbda86e1ea4edd842c4d
jisoo123456 <imearth0913@gmail.com>
2023-01-20 16:18:22 +0900

    10.2

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-# 10.1
-class Thing():
-    pass
+class Thing2():
+    letters = 'abc'
 
-example = Thing()
-print(Thing)  #출력값은 <class '__main__.Thing'> (class이기 때문)
-print(example)  #출력값은 <__main__.Thing object at 0x000002104AEBA1C0>으로 다르다.
+print(Thing2().letters)
