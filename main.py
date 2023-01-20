2fa087359aff5c09c416e51ecc6ac668f72ce240
jisoo123456 <imearth0913@gmail.com>
2023-01-20 17:07:15 +0900

    10.5

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-# 10.6
 class Element():
     def __init__(self, name, symbol, number):
         self.name = name
         self.symbol = symbol
         self.number = number
 
-    def dump(self):
-        print(f"name: {self.name}, symbol: {self.symbol}, number: {self.number}")
-
-hydrogen = Element('Hydrogen', 'H', 1)
-hydrogen.dump()
+el_dict = {'name': 'Hydrogen', 'symbol': 'H', 'number': 1}
+hydrogen = Element(el_dict['name'], el_dict['symbol'], el_dict['number'])
