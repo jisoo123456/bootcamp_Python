e10b116846b4321cd035a901c862d01061077e8d
jisoo123456 <imearth0913@gmail.com>
2023-01-20 18:35:11 +0900

    10.8

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-# 10.7
+# 10.8
 class Element():
     def __init__(self, name, symbol, number):
         self.name = name
         self.symbol = symbol
         self.number = number
-
-    def __str__(self):
-        return (f"name: {self.name}, symbol: {self.symbol}, number: {self.number}")
+    @property
+    def Name(self):
+        return self.name
+    @property
+    def Symbol(self):
+        return self.symbol
+    @property
+    def Number(self):
+        return self.number
 
 hydrogen = Element('Hydrogen', 'H', 1)
-print(hydrogen)  #이거는 앞에 hyerogen.dump()처럼 함수를 호출하지 않아도 자동으로 반환되서 문자열이 출력된다.
+print(hydrogen.Name)
+print(hydrogen.Symbol)
+print(hydrogen.Number)
