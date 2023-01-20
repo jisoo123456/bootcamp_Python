4fe2c2cbe3435225badfa9870f4e39490c37d878
jisoo123456 <imearth0913@gmail.com>
2023-01-20 18:47:59 +0900

    10.9

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
-# 10.8
-class Element():
-    def __init__(self, name, symbol, number):
-        self.name = name
-        self.symbol = symbol
-        self.number = number
-    @property
-    def Name(self):
-        return self.name
-    @property
-    def Symbol(self):
-        return self.symbol
-    @property
-    def Number(self):
-        return self.number
+# 10.9
+class Bear():
+    def eats(self):
+        return 'berries'
+class Rabbit():
+    def eats(self):
+        return 'clover'
+class Octothorpe():
+    def eats(self):
+        return 'campers'
 
-hydrogen = Element('Hydrogen', 'H', 1)
-print(hydrogen.Name)
-print(hydrogen.Symbol)
-print(hydrogen.Number)
+bear = Bear()
+rabbit = Rabbit()
+octothorpe = Octothorpe()
+
+print(bear.eats(), rabbit.eats(), octothorpe.eats())
