5c0841ea0f20eba5fb7f47c05c3defbc4d702e95
jisoo123456 <imearth0913@gmail.com>
2023-01-20 16:51:16 +0900

    10.4

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-# 10.3
-class Thing3():
-    def __init__(self):
-        self.letters = 'xyz'
+# 10.4
+class Element():
+    def __init__(self, name, symbol, number):
+        self.name = name
+        self.symbol = symbol
+        self.number = number
 
 
-print(Thing3.letters)  #클래스를 만들지 못했기 때문에 __init__에 접근하지 못한다.
-print(Thing3().letters)  #init에 접근할때는 다음과 같이 ()클래스를 만들고 불러오기
+Hydrogen = Element('Hydorgen', 'H', 1)
