14bd611a5f129c09b69975bd662e05a083b422f1
jisoo123456 <imearth0913@gmail.com>
2023-02-02 17:23:52 +0900

    10.10

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# 10.9
-class Bear():
-    def eats(self):
-        return 'berries'
-class Rabbit():
-    def eats(self):
-        return 'clover'
-class Octothorpe():
-    def eats(self):
-        return 'campers'
+#10.10
+class Laser():
+    def does(cls):
+        return "disintegrate"
 
-bear = Bear()
-rabbit = Rabbit()
-octothorpe = Octothorpe()
+class Claw():
+    def does(self):
+        return "crush"
 
-print(bear.eats(), rabbit.eats(), octothorpe.eats())
+class SmartPhone():
+    def does(self):
+        return "ring"
+
+class Robot():
+    def __init__(self):
+        self.laser = Laser()
+        self.claw = Claw()
+        self.smartphone = SmartPhone()
+    def does(self):
+       return self.laser.does(), self.claw.does(), self.smartphone.does()
+
+a = Robot()
+print(a.does())
